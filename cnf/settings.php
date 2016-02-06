df985fe72773edea08fb12c25b4e9a4063cb9977
Alina Mackenzie <alina.mackenzie@gmail.com>
2016-02-05 20:28:05 -0600

    Initial commit

---
+++ cnf/settings.php
@@ -0,0 +1,11 @@
+<?php
+$db_url='mysql://default:default@localhost/default';
+$databases=array('default' => array(
+    'default' => array(
+        'database' => 'default',
+        'username' => 'default',
+        'password' => 'default',
+        'driver' => 'mysql',
+        'host' => 'localhost',
+    ),
+),);
