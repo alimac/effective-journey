c4ede67727efdb32acdcdef7b4119229ba863307
Alina Mackenzie <alina.mackenzie@gmail.com>
2016-02-07 17:41:52 -0600

    Address PHP coder sniffer errors

--- modules/hello_world/node--hello-world.tpl.php
+++ modules/hello_world/node--hello-world.tpl.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @file
+ * Render message at node level.
+ */
 
-//Render 'Content starts here!' message
+/**
+ * Render 'Content starts here!' message.
+ */
 print render($content["content_starts_here"]);
